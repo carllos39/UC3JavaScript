7367b6ad9552175f4efe92f78701db2712ab9510
carllos39 <140573083+carllos39@users.noreply.github.com>
2024-11-26 22:09:39 -0300

    atualizar

---
+++ 07/script.js
@@ -0,0 +1,26 @@
+
+
+function saudacao(nome){
+
+alert("Olá" +  nome)
+}
+
+function calculaOdobro(valorOriginal){
+    let dobro= valorOriginal * 2;
+    return dobro;
+}
+
+// let resposta=calculaOdobro(7);
+// alert(resposta);
+
+
+// saudacao("Carlos");
+
+
+
+function multiplicar(a, b) {    
+    return a * b;
+}
+
+let resultado = multiplicar(4, 5);
+console.log(resultado);
