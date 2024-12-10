4d00a16bccc817739cc55e59cab02901a8b3f3ea
carllos39 <carllos2005@outlook.com.br>
2024-12-10 17:35:58 -0300

    calculadora

---
+++ projetos/calculadora/js/calculadora.js
@@ -0,0 +1,52 @@
+let display = document.getElementById("display");
+let currentInput = "";
+let operator = "";
+let firstOperand = ""; 
+
+function appendNumber(number){
+    currentInput += number;
+    display.value = currentInput;
+}
+function appendOperator(op) {
+    if (currentInput === '') return;
+    firstOperand = currentInput;
+    operator = op;
+    currentInput = "";
+
+}
+
+function calculateResult() {
+    if (firstOperand === '' || currentInput === '') return;
+    let result;
+
+    const num1 = parseFloat(firstOperand);
+    const num2 = parseFloat(currentInput);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+            default:
+            return;
+    }
+    display.value = result;
+    currentInput= result.toString();
+    operator='';
+    firstOperand='';
+}
+
+function clearDisplay(){
+    currentInput='';
+    operator ='';
+    firstOperand='';
+    display.value='';
+}
